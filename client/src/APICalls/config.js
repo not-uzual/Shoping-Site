0e001157e96e66188af96e758f48b861a683a823
Ujjwal Sahu <ujjwalsahu19@gmail.com>
2026-02-06 12:57:42 +0530

    chore: api url toggle

--- client/src/APICalls/config.js
+++ client/src/APICalls/config.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = "https://shoping-site-3cui.onrender.com";
-export const API_BASE_URL = "http://localhost:8000"
+export const API_BASE_URL = "https://shoping-site-3cui.onrender.com";
+// export const API_BASE_URL = "http://localhost:8000"
